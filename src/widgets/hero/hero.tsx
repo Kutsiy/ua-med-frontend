6a504a8b82d7a9401c3f4661dfb30087b4318cc7
Egor <kycuj.egor2020@gmail.com>
2026-07-11 17:57:40 +0300

    feat(main-page): create hero widget

---
+++ src/widgets/hero/hero.tsx
@@ -0,0 +1,9 @@
+export function Hero() {
+  return (
+    <div className="w-full h-[450px] flex items-center justify-center main-container">
+      <div className="px-2 p-1.5 rounded-lg bg-muted">
+        Понад 6 000 аптек та 12 000 лікарів по Україні
+      </div>
+    </div>
+  );
+}
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,1 +1,3 @@
-export { Header } from './header';
+export * from './header';
+export * from './main-layout';
+export * from './hero';
