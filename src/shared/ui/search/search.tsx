0b1da8d1841623f67c4313d23178f216bbc6cd21
Egor <kycuj.egor2020@gmail.com>
2026-07-13 20:52:40 +0300

    feat(main-page): create basic hero

--- src/shared/ui/search/search.tsx
+++ src/shared/ui/search/search.tsx
@@ -4,13 +4,16 @@ import { Button } from '../button';
 
 export function Search() {
   return (
-    <InputGroup className="">
-      <InputGroupInput placeholder="Search..." />
-      <InputGroupAddon>
-        <SearchIcon />
+    <InputGroup className="w-[70%] h-16 py-2">
+      <InputGroupInput placeholder="Назва ліків, клінік або послуг" />
+      <InputGroupAddon className="px-2">
+        <SearchIcon className="size-6" />
       </InputGroupAddon>
-      <InputGroupAddon align="inline-end">
-        <Button>Знайти</Button>
+      <InputGroupAddon
+        className="relative before:absolute before:left-[-10px] before:content-[''] before:h-[90%] before:w-[2px] before:bg-secondary"
+        align="inline-end"
+      >
+        <Button className="h-12 text-3xl">Знайти</Button>
       </InputGroupAddon>
     </InputGroup>
   );
